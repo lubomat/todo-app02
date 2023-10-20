ca674765b43a822d324bb62b93eb328086d8058d
Lubowiecki Mateusz <matlubowiecki@gmail.com>
2023-10-20 12:14:30 +0200

    crud done

--- src/main/java/com/todoapp02/todoapp02/TodoApp02Application.java
+++ src/main/java/com/todoapp02/todoapp02/TodoApp02Application.java
@@ -2,12 +2,22 @@ package com.todoapp02.todoapp02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.xml.validation.Validator;
 
 @SpringBootApplication
-public class TodoApp02Application {
+public class TodoApp02Application implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApp02Application.class, args);
 	}
 
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+		RepositoryRestConfigurer.super.configureValidatingRepositoryEventListener(validatingListener);
+	}
 }

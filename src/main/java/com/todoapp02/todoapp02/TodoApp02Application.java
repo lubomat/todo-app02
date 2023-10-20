cac9c665667ad965a18ce22c9d5960e8693d3d6a
Lubowiecki Mateusz <matlubowiecki@gmail.com>
2023-10-20 11:16:28 +0200

    Initial commit

---
+++ src/main/java/com/todoapp02/todoapp02/TodoApp02Application.java
@@ -0,0 +1,13 @@
+package com.todoapp02.todoapp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApp02Application.class, args);
+	}
+
+}

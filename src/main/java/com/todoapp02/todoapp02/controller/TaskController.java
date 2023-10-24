37c1bc671e5307f49bf3bc18565c24d1b9ff8aa1
Lubowiecki Mateusz <matlubowiecki@gmail.com>
2023-10-24 17:07:13 +0200

    add @GetMapping with logger warn

--- src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
+++ src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
@@ -1,15 +1,27 @@
 package com.todoapp02.todoapp02.controller;
 
 import com.todoapp02.todoapp02.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RepositoryRestController
 public class TaskController {
 
+    public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
 
     TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<?> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
     }
 
 

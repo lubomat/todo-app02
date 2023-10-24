3e787910797ed7dd315f3f8799e4a1ec5052a493
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-10-24 19:00:01 +0200

    edit @GetMapping to pageable List<Task> + .getContent

--- src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
+++ src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.todoapp02.todoapp02.controller;
 
+import com.todoapp02.todoapp02.model.Task;
 import com.todoapp02.todoapp02.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @RepositoryRestController
@@ -20,15 +23,15 @@ public class TaskController {
     }
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
-    ResponseEntity<?> readAllTasks() {
+    ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
     @GetMapping("/tasks")
-    ResponseEntity<?> readAllTasks(Pageable page) {
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
-        return ResponseEntity.ok(repository.findAll(page));
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
 

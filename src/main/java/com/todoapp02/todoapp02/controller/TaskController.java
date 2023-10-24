742a3433a79c4eb7f9c3335270d5563c31e45d8f
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-10-24 18:51:05 +0200

    add another @GetMapping with pageable

--- src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
+++ src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.todoapp02.todoapp02.controller;
 import com.todoapp02.todoapp02.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,12 @@ public class TaskController {
     ResponseEntity<?> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page));
     }
 
 

f979e7514352a6acca76ed7f62fd1f319d125bb3
Lubowiecki Mateusz <matlubowiecki@gmail.com>
2023-10-25 10:57:57 +0200

    many changes

--- src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
+++ src/main/java/com/todoapp02/todoapp02/controller/TaskController.java
@@ -5,7 +5,6 @@ import com.todoapp02.todoapp02.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-public class TaskController {
+class TaskController {
 
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
--- src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
@@ -1,15 +1,20 @@
 package com.todoapp02.todoapp02.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.stereotype.Repository;
 
+import java.net.ContentHandler;
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository {
+    List<Task> findAll();
+    Page<Task> findAll(Pageable page);
+    Optional<Task> findById(Integer id);
+
+    Task save(Task entity);
 
     List<Task> findByDone(@Param("state") boolean done);
+
 }

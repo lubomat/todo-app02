a1f2de02c0b036266112aa3d5f44640fa509e0d9
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-02 11:58:20 +0100

    CRUD STOP.. 2

--- src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
@@ -3,6 +3,7 @@ package com.todoapp02.todoapp02.model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;

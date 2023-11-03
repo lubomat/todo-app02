3b7748d5443385fe9fa629de078af607f8af485e
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-03 13:26:08 +0100

    nativeQuery in SqlTR delete param in TaskRepository

--- src/main/java/com/todoapp02/todoapp02/model/SqlTaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/SqlTaskRepository.java
@@ -1,10 +1,16 @@
 package com.todoapp02.todoapp02.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
-
+    @Override
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
 }

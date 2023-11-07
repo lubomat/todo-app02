a5f9117218933dbe078791f4f6f6a83ef12a1d1d
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-07 13:38:47 +0100

    edit SqlTaskRepository edit TaskRepository

--- src/main/java/com/todoapp02/todoapp02/model/SqlTaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/SqlTaskRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
     @Override
     @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
     boolean existsById(@Param("id") Integer id);
+
+    @Override
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);   // pozwala po nie zamknietym
+                                                               // zadaniu z grupa o zadanym
+                                                               // id odfiltrowac
 }
--- src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
@@ -17,6 +17,8 @@ public interface TaskRepository {
 
     boolean existsById(Integer id);
 
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
+
     List<Task> findByDone(boolean done);
 
     Task save(Task entity);

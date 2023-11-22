37234389b0e261b059a639200f0a46be3f09287e
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-22 17:06:10 +0100

    edit LogicConfiguration edit TaskGroupService

--- src/main/java/com/todoapp02/todoapp02/logic/LogicConfiguration.java
+++ src/main/java/com/todoapp02/todoapp02/logic/LogicConfiguration.java
@@ -3,17 +3,26 @@ package com.todoapp02.todoapp02.logic;
 import com.todoapp02.todoapp02.TaskConfigurationProperties;
 import com.todoapp02.todoapp02.model.ProjectRepository;
 import com.todoapp02.todoapp02.model.TaskGroupRepository;
+import com.todoapp02.todoapp02.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogicConfiguration {
-    @Bean
-    ProjectService service(
+    @Bean                                        // @Service  ProjectService
+    ProjectService projectService(
             final ProjectRepository repository,
             final TaskGroupRepository taskGroupRepository,
             final TaskConfigurationProperties config
     ) {
         return new ProjectService(repository, taskGroupRepository, config);
+    }
+
+    @Bean
+    TaskGroupService taskGroupService(
+            final TaskGroupRepository repository,
+            final TaskRepository taskRepository
+    ) {
+        return new TaskGroupService(repository, taskRepository);
     }
 }

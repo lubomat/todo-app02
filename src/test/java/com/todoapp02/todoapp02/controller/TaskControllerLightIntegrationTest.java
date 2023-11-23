b09ae2a3b9699e6148c296c9ec014555fa1046a5
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-23 16:40:25 +0100

    Create TaskControllerLightIntegrationTest

---
+++ src/test/java/com/todoapp02/todoapp02/controller/TaskControllerLightIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.todoapp02.todoapp02.controller;
+
+import com.todoapp02.todoapp02.model.Task;
+import com.todoapp02.todoapp02.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(TaskController.class)
+public class TaskControllerLightIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        //given
+        String description = "foo";
+        when(repo.findById(anyInt()))
+                .thenReturn(Optional.of(new Task(description, LocalDateTime.now())));
+
+        // when + then
+        mockMvc.perform(get("/tasks/123"))
+                .andDo(print())
+                .andExpect(content().string(containsString(description)));
+
+    }
+
+}
+
+

3ef79111f8ec73ae17368e13416a74659eafd8d3
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-11-30 16:56:43 +0100

    ACTUATOR

--- src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
+++ src/main/java/com/todoapp02/todoapp02/model/TaskRepository.java
@@ -2,8 +2,6 @@ package com.todoapp02.todoapp02.model;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
